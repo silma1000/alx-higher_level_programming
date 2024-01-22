5df659519ef4108d16391d410202d210f71ebd06
silma1000 <jaguhdeveloper@gmail.com>
2024-01-21 21:39:31 -0800

    loops

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,7 +2,7 @@
 start, end = 97, 123
 while start < end :
     if start == 101 or start == 113:
-        start += 1
+        start
     else:
         print("{}".format(chr(start)), end="")
     start += 1
